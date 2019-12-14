a444f8bdcd21e73f8fa6e773a48eec591e0cf0ba
weeklavya <ravibiradar2811@gmail.com>
2019-12-14 12:22:56 +0530

    basic codes
    downloader from internate

---
+++ 1.py
@@ -0,0 +1,35 @@
+ 
+
+import os
+import pytesseract
+from PIL import Image
+import tempfile
+import subprocess
+import cv2
+import time
+cam = cv2.VideoCapture(0)
+ 
+
+
+ret = True
+while ret:
+    ret, img = cam.read()
+    img = cv2.imread('2.jpg',cv2.IMREAD_COLOR) #Open the image from which charectors has to be recognized
+    #img = cv2.resize(img, (620,480) ) #resize the image if required
+    cv2.imshow('img',img)
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey to reduce detials
+    gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+
+    original = pytesseract.image_to_string(gray, config='eng' )
+    test = (pytesseract.image_to_data(gray, lang=None, config='-l eng --oem 3 --psm 12') ) #get confidence level if required
+    print(test)
+
+    print (original)    
+    if 0xFF & cv2.waitKey(5) == 27:
+        break
+
+cv2.destroyAllWindows()
+ 
+ 
+
